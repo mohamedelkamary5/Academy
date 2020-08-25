c2fad0602ecf4ac6475c7ba8dd3bb2dce5627f0a
MohamedElkamary2 <amo263128@gmail.com>
2020-08-25 23:59:03 +0200

    Updates

---
+++ precache-manifest.af449e5072ced2e17abc9506dc327f9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c839909cdc6339a1143cf799081e89",
+    "url": "/Academy/index.html"
+  },
+  {
+    "revision": "57e3f43dac8b1f35ac17",
+    "url": "/Academy/static/css/2.3d876496.chunk.css"
+  },
+  {
+    "revision": "fd3bcb81004ddf5eb134",
+    "url": "/Academy/static/css/main.080cd321.chunk.css"
+  },
+  {
+    "revision": "57e3f43dac8b1f35ac17",
+    "url": "/Academy/static/js/2.46fd1bd0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Academy/static/js/2.46fd1bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3bcb81004ddf5eb134",
+    "url": "/Academy/static/js/main.49ae8fe5.chunk.js"
+  },
+  {
+    "revision": "cb44577ddb3ac0f8edf0",
+    "url": "/Academy/static/js/runtime-main.b280fc2c.js"
+  },
+  {
+    "revision": "423d6cc5e04aac5944b154ead2eb0130",
+    "url": "/Academy/static/media/pexels.423d6cc5.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Academy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Academy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Academy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Academy/static/media/slick.f97e3bbf.svg"
+  }
+]);
